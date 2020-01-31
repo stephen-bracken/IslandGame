a48d881f309a4ed5c9f94dbbf85b10169ce9daa1
InfernoKay <InfernoKay@users.noreply.github.com>
2020-01-31 22:18:07 +0000

    Camera Rotation Script
    For Play Testing,
    A and D rotate camera 45 degrees

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    private Vector3 point = new Vector3(0.0f, 0.0f, 0.0f);
+    private float speed = 100.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), 45);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), -45);
+        }
+    }
+}
