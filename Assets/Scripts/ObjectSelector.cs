c553d2a6a263532a064684fdff31b2015799c379
InfernoKay <InfernoKay@users.noreply.github.com>
2020-02-01 13:41:23 +0000

    Moving Blocks
    Actually moves the blocks
    Doesn't snap to grid yet

--- Assets/Scripts/ObjectSelector.cs
+++ Assets/Scripts/ObjectSelector.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ObjectSelector : MonoBehaviour
 {
     public float force = 5.0f;
+    public bool isSelected = false;
+    Rigidbody rb;
 
     void FixedUpdate()
     {
@@ -17,14 +19,23 @@ public class ObjectSelector : MonoBehaviour
             {
                 if (hit.transform != null)
                 {
-                    Rigidbody rb;
                     PrintName(hit.transform.gameObject);
                     if (rb = hit.transform.GetComponent<Rigidbody>())
                     {
-                        moveBody(rb);
+                        isSelected = true;
+                        //moveBody(rb);
                     }
                 }
+                else
+                {
+                    isSelected = false;
+                }
             }
+        }
+
+        if (isSelected && rb != null)
+        {
+            moveBody(rb);
         }
     }
 
@@ -39,31 +50,31 @@ public class ObjectSelector : MonoBehaviour
         // Z axis
         if (Input.GetKeyDown("q"))
         {
-            rig.AddForce(transform.forward * force);
+            rig.AddForce(transform.forward * force, ForceMode.Impulse);
         }
         if (Input.GetKeyDown("a"))
         {
-
+            rig.AddForce(-transform.forward * force, ForceMode.Impulse);
         }
 
         // Y axis
         if (Input.GetKeyDown("w"))
         {
-
+            rig.AddForce(transform.up * force, ForceMode.Impulse);
         }
         if (Input.GetKeyDown("s"))
         {
-
+            rig.AddForce(-transform.up * force, ForceMode.Impulse);
         }
 
         // X axis
         if (Input.GetKeyDown("d"))
         {
-
+            rig.AddForce(transform.right * force, ForceMode.Impulse);
         }
         if (Input.GetKeyDown("e"))
         {
-
+            rig.AddForce(-transform.right * force, ForceMode.Impulse);
         }
     }
 }
