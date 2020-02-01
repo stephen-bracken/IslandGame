b65ad23b16814493c9327f1922e93633f807e144
InfernoKay <InfernoKay@users.noreply.github.com>
2020-02-01 12:52:49 +0000

    Organisation
    Organised into Files
    Started work on grid

---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{   
+    void FixedUpdate()
+    {
+        var currentPos = transform.position;
+        transform.position = new Vector3(Mathf.Round(currentPos.x),
+                                     Mathf.Round(currentPos.y),
+                                     Mathf.Round(currentPos.z));
+    }
+}
