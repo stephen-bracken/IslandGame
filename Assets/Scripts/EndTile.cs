6526337179136a8c99c60ce2c1df9896c491d462
InfernoKay <InfernoKay@users.noreply.github.com>
2020-02-02 12:04:11 +0000

    Created Endtile
    End tile created
    Moves bridge that is linked by user inputted amount

---
+++ Assets/Scripts/EndTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTile : MonoBehaviour
+{
+    public Transform toMove;
+    public float moveX = 0f;
+    public float moveZ = 25f;
+    private bool moved = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = Color.green;
+
+        if (!moved)
+        {
+            toMove.transform.position += new Vector3(moveX, 0f, moveZ);
+            moved = true;
+        }
+    }
+}
